8537a055c5a9a28d09507948043556711c24244f
basamaryan <basam.aryan@gmail.com>
2024-07-22 20:09:39 +0530

    vince: Enable TARGET_USES_DOLBY
    Change-Id: I33d799f4a9f3b8fe05dbfcfa3d623049da187f00

--- device.mk
+++ device.mk
@@ -139,6 +139,9 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     hwcomposer.qcom
 
+# Dolby
+TARGET_USES_DOLBY := true
+
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 

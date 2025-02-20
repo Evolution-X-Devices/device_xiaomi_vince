3dbb77bb659af39d11eddfae4b3f49f789c26609
k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>
2025-02-20 18:26:49 +0700

    Revert "vince: Fix goodix fingerprint"
    This reverts commit 501575cdfcfaf581516e5ecb432c9d6dc5b215c0.

--- extract-files.sh
+++ extract-files.sh
@@ -90,12 +90,9 @@ function blob_fixup() {
         vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
             "${PATCHELF_0_17_2}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
             ;;
-	vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so)
-            "${PATCHELF_0_17_2}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
-	        ;;
     esac
 
-    # For all ELF files
+# For all ELF files
     if [[ "${1}" =~ ^.*(\.so|\/bin\/.*)$ ]]; then
         "${PATCHELF_0_17_2}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
     fi

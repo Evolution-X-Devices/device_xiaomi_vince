b7a88010f3cbec0e843a4de9a05fb9e8bd281ed0
Onelots <onelots@onelots.fr>
2025-05-01 00:08:26 +0200

    vince: do not build Graphene cam
    Signed-off-by: Onelots <onelots@onelots.fr>

--- device.mk
+++ device.mk
@@ -57,10 +57,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     AntHalService
 
-# Prebuilt
-PRODUCT_PACKAGES += \
-    Camera
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \

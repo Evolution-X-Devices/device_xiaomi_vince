91f737d18a8be9b969e0f05b1c3dcc40e960cf66
Onelots <onelots@onelots.fr>
2025-06-05 09:21:15 +0200

    vince: specify platform
    * Messes up with 8996 for some reason
    
    Signed-off-by: Onelots <onelots@onelots.fr>

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -124,7 +124,7 @@ LOCAL_STATIC_LIBRARIES := android.hardware.camera.common@1.0-helper
 
 
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := camera.msm8953
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_TAGS := optional
 
